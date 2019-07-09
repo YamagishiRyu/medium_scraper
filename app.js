cdf61e5a77006fa54d7d5f3ec7514e0563ec0427
YamagishiRyu <bubusu.ryu@gmail.com>
2019-07-09 20:06:22 +0900

    :zap: Add: codes to get json

--- app.js
+++ app.js
@@ -24,19 +24,29 @@ const puppeteer = require('puppeteer');
         repeatDelayMs: 200
       }
     );
-  console.log('scrolled')
+  console.log('scrolled');
 
-  // get post information
+  // get url information
   const post_selector = '#root > div > section > section.y.cs.ab.n > div:nth-child(3) > div.ez.fa.n > section'
-  posts = page.$$eval(post_selector, nodes => nodes.map(element => {
+  post_info_array = await page.$$eval(post_selector, nodes => nodes.map(element => {
     return {
       title: element.querySelector('h3').innerText,
       url: element.querySelector('h3 a').href
     }
   }));
+  console.log('post info done');
 
-  console.log(await posts)
+  // get post for post page
 
+  for(var i = 0, l = post_info_array.length; i < l; i++){
+    url = post_info_array[i].url;
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    await page.waitFor(1000);
+    await page.mouse.click(10,10);
+    
+    info_json = await page.$eval('head > script[type="application/ld+json"]', el => el.innerText);
+    console.log(info_json)
+  }
 
   await browser.close();
 })();
