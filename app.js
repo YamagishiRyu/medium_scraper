561a5c1947ad9c249e541cc0131dd29ca497cbb4
YamagishiRyu <bubusu.ryu@gmail.com>
2019-07-09 20:55:51 +0900

    :zap: Add: csv output

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -12,18 +13,18 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 2000 });
   await page.goto('https://medium.com/topic/popular');
-  await page._client.send(
-      'Input.synthesizeScrollGesture',
-      {
-        x: 0,
-        y: 0,
-        xDistance: 0,
-        yDistance: -3500,
-        speed: 2000,
-        repeatCount: 5,
-        repeatDelayMs: 200
-      }
-    );
+//  await page._client.send(
+//      'Input.synthesizeScrollGesture',
+//      {
+//        x: 0,
+//        y: 0,
+//        xDistance: 0,
+//        yDistance: -3500,
+//        speed: 2000,
+//        repeatCount: 5,
+//        repeatDelayMs: 200
+//      }
+//    );
   console.log('scrolled');
 
   // get url information
@@ -35,18 +36,32 @@ const puppeteer = require('puppeteer');
     }
   }));
   console.log('post info done');
+  console.log('post data: ' + post_info_array.length);
 
   // get post for post page
 
+  var file = fs.createWriteStream('medium_posts.csv', {flags: 'a+'});
+  file.write('id,title,author_name,texts,keywords\n')
   for(var i = 0, l = post_info_array.length; i < l; i++){
     url = post_info_array[i].url;
     await page.goto(url, {waitUntil: 'networkidle2'});
     await page.waitFor(1000);
     await page.mouse.click(10,10);
     
-    info_json = await page.$eval('head > script[type="application/ld+json"]', el => el.innerText);
-    console.log(info_json)
+    info_json = await page.$eval('head > script[type="application/ld+json"]', el => JSON.parse(el.innerText));
+    id = info_json.articleId;
+    title = info_json.name;
+    author_name = info_json.author.name;
+    keywords = info_json.keywords.join('|')
+    texts = await page.$$eval('article p', nodes => nodes.map(element => {
+      return element.innerText;
+    }))
+    text = texts.join(' ');
+
+    file.write(id + ',†' + title + '†,' + author_name + ',†' + text + '†,' + keywords + '\n');
   }
+
+  file.end();
 
   await browser.close();
 })();
