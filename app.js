68e8894b2155c8996def2bf0ed2f90abd612bef5
YamagishiRyu <bubusu.ryu@gmail.com>
2019-07-10 21:10:42 +0900

    :zap: Update: dom selector

--- app.js
+++ app.js
@@ -13,22 +13,22 @@ const fs = require('fs');
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 2000 });
   await page.goto('https://medium.com/topic/popular');
-//  await page._client.send(
-//      'Input.synthesizeScrollGesture',
-//      {
-//        x: 0,
-//        y: 0,
-//        xDistance: 0,
-//        yDistance: -3500,
-//        speed: 2000,
-//        repeatCount: 5,
-//        repeatDelayMs: 200
-//      }
-//    );
+  await page._client.send(
+      'Input.synthesizeScrollGesture',
+      {
+        x: 0,
+        y: 0,
+        xDistance: 0,
+        yDistance: -3500,
+        speed: 2000,
+        repeatCount: 5,
+        repeatDelayMs: 200
+      }
+    );
   console.log('scrolled');
 
   // get url information
-  const post_selector = '#root > div > section > section.y.cs.ab.n > div:nth-child(3) > div.ez.fa.n > section'
+  const post_selector = '#root > div > section > section.y.cs.ab.n > div:nth-child(3) > div > section'
   post_info_array = await page.$$eval(post_selector, nodes => nodes.map(element => {
     return {
       title: element.querySelector('h3').innerText,
